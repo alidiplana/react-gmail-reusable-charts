4227bc11ec71ac9278ec5cd9d4c7a81334828b9b
Muhammad Ali <alidiplana@gmail.com>
2021-07-30 00:45:38 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+
+export class App extends Component {
+  responseGoogle = (response) => {
+
+    console.log(response);
+    console.log(this.fetchDetail(response.tokenObj.access_token));
+    
+  };
+
+  fetchDetail(token) {
+    fetch(
+      "https://gmail.googleapis.com/gmail/v1/users/alidiplana@gmail.com/labels",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          
+        },
+      }
+    ).then(response => response.json())
+    .then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+          clientId="354361971363-8jjcvgd3brcgb2nn38bqmhi0gjr3e2rh.apps.googleusercontent.com"
+          onSuccess={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
